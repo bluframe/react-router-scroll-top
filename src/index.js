fccb6e4c51691d9b39e75c440428b5e6744907fd
Unknown <edoardo@blufra.me>
2018-10-13 16:48:52 +0100

    PureComponent
    And Flow Updates

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 // @flow
 
-import { type Element } from "react"
 import ScrollToTop from "./ScrollToTop"
 import { withRouter } from "react-router-dom"
 
 const TOP = 0
 const LEFT = 0
 
-export type Props = {
-  children?: Element<*>
+type Location = {
+  +key: string,
+  +pathname: string,
+  +search: string,
+  +hash: string,
+  +state: {
+    +[string]: boolean
+  }
 }
 
-export const componentDidUpdate = function componentDidUpdate(prevProps: Props) {
-  const { location } = this.props
+export type Props = {
+  children?: React$Node,
+  location: Location
+}
+
+export const componentDidUpdate = function componentDidUpdate(
+  prevProps: Props
+) {
+  const { location }: Props = this.props
 
   if (location !== prevProps.location) {
     window.scrollTo(LEFT, TOP)
